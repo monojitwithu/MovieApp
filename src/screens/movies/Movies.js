8077c133ea7affff982bae7a38704d5cda404b24
monojitwithu <84927543+monojitwithu@users.noreply.github.com>
2022-01-28 03:10:51 +0530

    Update Movies.js

--- src/screens/movies/Movies.js
+++ src/screens/movies/Movies.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react"
+import React, { useEffect,useState } from "react"
 import Card from "../../component/common/card/Card";
 import Header from "../../component/common/header/Header";
 
